15d2edd62162fef461727f4e8da078f2c042ecba
Shree Majumder <smaju002@fiu.edu>
2020-02-13 20:37:29 -0500

    learning

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          first react......
         </a>
       </header>
     </div>
