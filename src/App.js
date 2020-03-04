ce05a6ed881c1ae8d683247c89b7e6f64431be23
Hamilton Chevez <hchev001@gmail.com>
2020-03-03 20:03:57 -0500

    Created A theme provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import indexRoutes from "./routes";
 
-import { Route, Switch} from "react-router-dom";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Theme from "./Theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
   return (
-    
     <div style={{ minHeight: "100vh" }}>
-      <CssBaseline />
-      <Switch>
-        {indexRoutes.map((prop, key) => {
-          return (<Route path={prop.path} key={key} component={prop.component} />)
-        })}
-      </Switch>
+      <ThemeProvider theme={Theme}>
+        <CssBaseline />
+        <Switch>
+          {indexRoutes.map((prop, key) => {
+            return (
+              <Route path={prop.path} key={key} component={prop.component} />
+            );
+          })}
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
