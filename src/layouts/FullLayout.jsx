2845e51345db5244e471c39567654159089eafb2
Hamilton Chevez <hchev001@gmail.com>
2020-03-03 20:21:45 -0500

    modified the appbar to match the xd ui

--- src/layouts/FullLayout.jsx
+++ src/layouts/FullLayout.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,69 +8,112 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import MenuIcon from "@material-ui/icons/Menu";
-import {publicRoutes } from "../routes/allRoutes";
+import { publicRoutes } from "../routes/allRoutes";
 import styled from "styled-components";
-import { Route, Switch} from "react-router-dom";
-
-
+import { Route, Switch } from "react-router-dom";
+import Link from "@material-ui/core/Link";
 
 const Side = styled.div`
   width: 250;
 `;
 
+const StyledToolbar = styled(Toolbar)`
+  display: flex;
+  justify-content: space-between;
 
+  .buttonsOnRight {
+    display: flex;
+    justify-content: space-around;
+    min-width: 400px;
+  }
+`;
 
-const FullLayout = (props) => {
-    const [drawerOpen, setDrawerOpen] = useState(false);
+const StyledLink = styled(Link)`
+  color: #ffffff;
+  text-decoration: none;
+  font-size: 1rem;
+`;
 
-    const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+const FullLayout = props => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-    const switchRoutes = (route) => {
-        props.history.push(route);
-        toggleDrawer();
-    }
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
 
+  const switchRoutes = route => {
+    props.history.push(route);
+    toggleDrawer();
+  };
 
-    return (
-        <div style={{minHeight: "100vh", position: "relative"}}>
-            <AppBar position="static">
-              <Toolbar>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  onClick={() => toggleDrawer()}
-                >
-                  <MenuIcon />
-                </IconButton>
-
-              </Toolbar>
-            </AppBar>
-            <Drawer open={drawerOpen} onClose={toggleDrawer}>
-              <Side>
-                <List component="nav">
-                  {publicRoutes.map(route => (
-                    <ListItem
-                      button
-                      onClick={() => switchRoutes(route.path)}
-                      key={route.key}
-                    >
-                      <ListItemIcon>
-                        {<route.icon color="primary" />}
-                      </ListItemIcon>
-                      <ListItemText primary={route.name} />
-                    </ListItem>
-                  ))}
-                </List>
-              </Side>
-            </Drawer>
-            <Switch>
-              {publicRoutes.map(publicRoutes => {
-                return (<Route exact  path={publicRoutes.path} key={publicRoutes.key} component={publicRoutes.component} />)
-              })}
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div style={{ minHeight: "100vh", position: "relative" }}>
+      <AppBar position="static">
+        <StyledToolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => toggleDrawer()}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className="buttonsOnRight">
+            <StyledLink
+              component="button"
+              onClick={() => {
+                console.info("I'm a button.");
+              }}
+            >
+              Explore
+            </StyledLink>
+            <StyledLink
+              component="button"
+              onClick={() => {
+                console.info("I'm a button.");
+              }}
+            >
+              Sign In
+            </StyledLink>
+            <StyledLink
+              component="button"
+              onClick={() => {
+                console.info("I'm a button.");
+              }}
+            >
+              Sign Up
+            </StyledLink>
+          </div>
+        </StyledToolbar>
+      </AppBar>
+      <Drawer open={drawerOpen} onClose={toggleDrawer}>
+        <Side>
+          <List component="nav">
+            {publicRoutes.map(route => (
+              <ListItem
+                button
+                onClick={() => switchRoutes(route.path)}
+                key={route.key}
+              >
+                <ListItemIcon>{<route.icon color="primary" />}</ListItemIcon>
+                <ListItemText primary={route.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Side>
+      </Drawer>
+      <Switch>
+        {publicRoutes.map(publicRoutes => {
+          return (
+            <Route
+              exact
+              path={publicRoutes.path}
+              key={publicRoutes.key}
+              component={publicRoutes.component}
+            />
+          );
+        })}
+      </Switch>
+    </div>
+  );
+};
 
 export default FullLayout;
