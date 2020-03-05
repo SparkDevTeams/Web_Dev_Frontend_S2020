3b59f9f8bd17da0fdfac18ac7d869d12e6e17e70
Maricela <mrami235@fiu.edu>
2020-03-04 21:05:23 -0500

    test

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { TablePagination } from "@material-ui/core";
+import styled from "styled-components";
+import {Actions} from 'react-native-router-flux'; 
+import { Component } from "react";
+import {Helmet} from 'react-helmet';
+
+class Application extends React.Component {
+    render() {
+        return( 
+             <div className="application" >
+                <Helmet>
+                    <style>
+                        {'body{ background-color: red;}'}
+                    </style>
+                </Helmet>
+            </div>
+        );
+    }
+};
+
+/*export default class Form extends Component{
+    constructor(props){
+      super(props){
+        this.state={
+          firstName:'',
+          lastName:''.
+          username:'',
+          email:'',
+          password:'',
+          confirmPassword:''
+        }
+      }
+    }
+}
+*/
+
+const SignUpPage = props => {
+    const [page, setPage] = React.useState(0);
+};
+
+
+export default SignUpPage;
