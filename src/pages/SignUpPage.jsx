15e7af10097aca11cbff09c31a0351e9039d03fe
Maricela <mrami235@fiu.edu>
2020-03-05 12:50:13 -0500

    sign up form added

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,15 +1,85 @@
 import React from "react";
 import {Helmet} from "react-helmet";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 
 
-export default function SignUpPage() {
+/*export default function SignUpPage() {
     return(
              <div >
                 <Helmet>
                     <style>
-                        {'body{ background-color: #202538;}'}
+                        {'body{ background-color: #202538c;}'}
                     </style>
                 </Helmet>
             </div>
     )
 }
+*/
+
+const FormPage = () => {
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard>
+            <MDBCardBody>
+              <form>
+                <p className="h4 text-center py-4">Sign up</p>
+                <div className="grey-text">
+                  <MDBInput
+                    label="Your name"
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your email"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Confirm your email"
+                    icon="exclamation-triangle"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                </div>
+                <div className="text-center py-4 mt-3">
+                  <MDBBtn color="cyan" type="submit">
+                    Register
+                  </MDBBtn>
+                </div>
+              </form>
+              <div >
+                <Helmet>
+                    <style>
+                        {'body{ background-color: #202538c;}'}
+                    </style>
+                </Helmet>
+            </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default FormPage;
