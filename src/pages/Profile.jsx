1b8714c740e410e98e3618bdd1954d54a9186375
Hamilton Chevez <hchev001@gmail.com>
2020-03-17 19:41:19 -0400

    profile page broke and signup page is being implemented

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,132 +1,164 @@
-import React, {useState} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Avatar from '@material-ui/core/Avatar';
-import CheckBoxRoundedIcon from '@material-ui/icons/CheckBoxRounded';
-import IndeterminateCheckBox from '@material-ui/icons/IndeterminateCheckBox';
-import CheckBoxOutlineBlankRoundedIcon from '@material-ui/icons/CheckBoxOutlineBlankRounded';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Avatar from "@material-ui/core/Avatar";
+import CheckBoxRoundedIcon from "@material-ui/icons/CheckBoxRounded";
+import IndeterminateCheckBox from "@material-ui/icons/IndeterminateCheckBox";
+import CheckBoxOutlineBlankRoundedIcon from "@material-ui/icons/CheckBoxOutlineBlankRounded";
 import { TablePagination } from "@material-ui/core";
 import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
 
-
 const ParentContainer = styled.div`
-    background-color: #202538;
-    height: 100%;
-`
-
+  background-color: #202538;
+  height: 100%;
+`;
 
 const AvatarContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: row;
-    padding: 8px 0px 8px 8px;
-    
-`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: row;
+  padding: 8px 0px 8px 8px;
+`;
 
 const StyledTableContainer = styled(TableContainer)`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    top-margin: 15px;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  top-margin: 15px;
+`;
 
 const StyledTable = styled(Table)`
-    max-width: 50%;
-    background-color: white;
+  max-width: 50%;
+  background-color: white;
 
-    th {
-        background-color: rgba(32, 37, 56, .6)
-    }
-`
+  th {
+    background-color: rgba(32, 37, 56, 0.6);
+  }
+`;
 const StyledTablePagination = styled(TablePagination)`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
 const StyledAvatar = styled(Avatar)`
-    height: 100px;
-    width: 100px;
-`
+  height: 100px;
+  width: 100px;
+`;
 const StyledGrid = styled(Grid)`
-    font-size: 33px;
-    padding: 0px 0px 0px 8px;
-`
-
+  font-size: 33px;
+  padding: 0px 0px 0px 8px;
+`;
 
 function getChallenges(name, date, status) {
-    return { name, date, status }
+  return { name, date, status };
 }
 const rows = [
-    getChallenges('test challenge 1', 2132020, <CheckBoxRoundedIcon />),
-    getChallenges('test challenge 2', 2132020, <IndeterminateCheckBox />),
-    getChallenges('test challenge 3', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
-    getChallenges('test challenge 4', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
-    getChallenges('test challenge 5', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
-    getChallenges('test challenge 6', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
-    getChallenges('test challenge 7', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
-    getChallenges('test challenge 8', 2132020, <CheckBoxOutlineBlankRoundedIcon />),
+  getChallenges("test challenge 1", 2132020, <CheckBoxRoundedIcon />),
+  getChallenges("test challenge 2", 2132020, <IndeterminateCheckBox />),
+  getChallenges(
+    "test challenge 3",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  ),
+  getChallenges(
+    "test challenge 4",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  ),
+  getChallenges(
+    "test challenge 5",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  ),
+  getChallenges(
+    "test challenge 6",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  ),
+  getChallenges(
+    "test challenge 7",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  ),
+  getChallenges(
+    "test challenge 8",
+    2132020,
+    <CheckBoxOutlineBlankRoundedIcon />
+  )
 ];
+
 const Profile = props => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [name, setName] = useState({ firstName: "Joe", lastName: "Schmoe"});
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-    const handleChangeRowsPerPage = event => {
-        setRowsPerPage(parseInt(+event.target.value));
-        setPage(0);
-    };
-    return (
-        <ParentContainer>
-            <AvatarContainer>
-                <StyledAvatar alt="profile pic" src="" />
-                <StyledGrid><h2>{name.firstName}</h2><h2>{name.lastName}</h2></StyledGrid>
-                
-            </AvatarContainer>
-            <StyledTableContainer>
-                <StyledTable stickyHeader aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Challenges Attempted</TableCell>
-                            <TableCell align="right">Date accessed</TableCell>
-                            <TableCell align="right">Status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {//for every entry in rows
-                            return (
-                                <TableRow key={row.name}>
-                                    <TableCell>{row.name}</TableCell>
-                                    <TableCell align="right">{row.date}</TableCell>
-                                    <TableCell align="right">{row.status}</TableCell>
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [userInfo, setUserInfo] = React.useState({});
+  const [name, setName] = useState({ firstName: "Joe", lastName: "Schmoe" });
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(+event.target.value));
+    setPage(0);
+  };
 
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </StyledTable>
-            </StyledTableContainer>
-            <StyledTablePagination
-                rowsPerPage={rowsPerPage}
-                rowsPerPageOptions={[5, 10, 100]}
-                component="div"
-                count={rows.length}
-                page={page}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}/>
-            
-            
-
-        </ParentContainer>
-    );
-}
-
+  useEffect(() => {
+    console.log("The component mounted");
+    try {
+      const userInStorage = JSON.parse(sessionStorage.getItem("user"));
+      setUserInfo(userInStorage);
+    } catch (error) {
+      console.log(error, "user is probably not logged in");
+    }
+  }, []);
+  return (
+    <ParentContainer>
+      <AvatarContainer>
+        <StyledAvatar alt="profile pic" src="" />
+        <StyledGrid>
+          <h2>{userInfo.name || `${name.firstName} ${name.lastName}`}</h2>
+        </StyledGrid>
+      </AvatarContainer>
+      <StyledTableContainer>
+        <StyledTable stickyHeader aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Challenges Attempted</TableCell>
+              <TableCell align="right">Date accessed</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => {
+                //for every entry in rows
+                return (
+                  <TableRow key={row.name}>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell align="right">{row.date}</TableCell>
+                    <TableCell align="right">{row.status}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </StyledTable>
+      </StyledTableContainer>
+      <StyledTablePagination
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10, 100]}
+        component="div"
+        count={rows.length}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </ParentContainer>
+  );
+};
 
 export default Profile;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -11,149 +11,190 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import styled from "styled-components";
-import Table from '@material-ui/core/Table';
-import TableContainer from '@material-ui/core/TableContainer';
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 const ParentContainer = styled.div`
-    background-color: #202538;
-    height: 100%;
-`
+  background-color: #202538;
+  height: 100%;
+`;
 const StyledTable = styled(Table)`
-    max-width: 50%;
-    background-color: white;
+  max-width: 50%;
+  background-color: white;
 
-    th {
-        background-color: rgba(32, 37, 56, .6)
-    }
-`
+  th {
+    background-color: rgba(32, 37, 56, 0.6);
+  }
+`;
 const StyledTableContainer = styled(TableContainer)`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    top-margin: 15px;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  top-margin: 15px;
+`;
 
 const useStyles = makeStyles(theme => ({
-    
-    paper: {
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(3)
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2)
-    }
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
+let schema = yup.object().shape({
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  email: yup
+    .string()
+    .email("Not a valid email")
+    .required(),
+  password: yup
+    .string()
+    .required()
+    .min(1)
+});
 
-export default function SignUp() {
-    const classes = useStyles();
+/**
+ * Requirements to sign up
+ * firstname
+ * lastname
+ * email
+ * password
+ */
+const SignUp = props => {
+  const classes = useStyles();
 
-    return ( 
-        <ParentContainer>
-        <StyledTableContainer>
-            <StyledTable>
-        <Container component="main" maxWidth="xs">
+  return (
+    <ParentContainer>
+      <StyledTableContainer>
+        <StyledTable>
+          <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <form className={classes.form} noValidate>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign up
+              </Typography>
+              <Formik
+                initialValues={{
+                  firstname: "",
+                  lastname: "",
+                  email: "",
+                  password: ""
+                }}
+                validationSchema={schema}
+                onSubmit={async (values, actions) => {
+                  try {
+                    // const response = await request({});
+                    console.log("we attempt to make a request with", values);
+                  } catch (error) {
+                    console.log(error);
+                  }
+                }}
+              >
+                {props => (
+                  <form className={classes.form} noValidate>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="confirmpassword"
-                                label="Confirm Password"
-                                type="confirmpassword"
-                                id="confirmpassword"
-                            />
-                        </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          autoComplete="fname"
+                          name="firstName"
+                          variant="outlined"
+                          onChange={props.handleChange}
+                          onBlur={props.handleBlur}
+                          value={props.values.firstname}
+                          required
+                          fullWidth
+                          id="firstName"
+                          label="First Name"
+                          autoFocus
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          variant="outlined"
+                          required
+                          fullWidth
+                          onChange={props.handleChange}
+                          onBlur={props.handleBlur}
+                          value={props.values.lastname}
+                          id="lastName"
+                          label="Last Name"
+                          name="lastName"
+                          autoComplete="lname"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          variant="outlined"
+                          required
+                          fullWidth
+                          onChange={props.handleChange}
+                          onBlur={props.handleBlur}
+                          value={props.values.email}
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          variant="outlined"
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          onChange={props.handleChange}
+                          onBlur={props.handleBlur}
+                          value={props.values.password}
+                          type="password"
+                          id="password"
+                          autoComplete="current-password"
+                        />
+                      </Grid>
                     </Grid>
                     <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
                     >
-                        Sign Up
+                      Sign Up
                     </Button>
                     <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="/login" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
+                      <Grid item>
+                        <Link href="/login" variant="body2">
+                          Already have an account? Sign in
+                        </Link>
+                      </Grid>
                     </Grid>
-                </form>
+                  </form>
+                )}
+              </Formik>
             </div>
-            <Box mt={5}>{/* <Copyright /> */}</Box> 
-        </Container>
+          </Container>
         </StyledTable>
-        </StyledTableContainer>
-        </ParentContainer>
-    );
-}
+      </StyledTableContainer>
+    </ParentContainer>
+  );
+};
 
+export default SignUp;
